39ec26630f6eaac42b17d2e4a5dd84b6b8f7fa65
Bat <kokomo31ads@gmail.com>
2026-01-13 20:38:50 -0500

    chore: Remove dead code across codebase
    Delete unused components:
    - Auth: LandingPage, SignInPage, SignUpPage (replaced by AuthModal)
    - Chat: Input, QuickActions, Error (never used)
    - UI: AIResponsePanel, BackgroundDecoration, CollapsedChatIcon,
      FloatingBrowserWindow, KnowledgeBaseToggle, LockScreen, WelcomeScreen
    - Pricing: PricingCard, PricingPage (entire folder)
    - Usage: UsageDisplay (entire folder)
    - Utils: dateUtils
    
    Update chat/index.js to remove dead exports

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -3,10 +3,7 @@
  */
 
 export { Messages } from './Messages';
-export { Input } from './Input';
 export { Composer } from './Composer';
-export { Error } from './Error';
 export { Sidebar } from './Sidebar';
 export { Banner } from './Banner';
-export { QuickActions } from './QuickActions';
 export { SearchModal } from './SearchModal';
